a8bb5dc30d0522e5b0417b1ae05299bd1dd3cf1f
Victor1492Alvarez <victorsamuel1492@gmail.com>
2026-06-08 23:32:50 +0200

    Refactor logger configuration in logging_utils.py
    Refactor logger configuration to avoid duplicate handlers and set output to stdout.

--- infrastructure/logging_utils.py
+++ infrastructure/logging_utils.py
@@ -1,13 +1,23 @@
 from __future__ import annotations
+
 import logging
+import sys
 
-def configure_logger(name: str = "ptmeoh_tool") -> logging.Logger:
+
+def configure_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
-    if not logger.handlers:
-        logger.setLevel(logging.INFO)
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter("%(asctime)s | %(name)s | %(levelname)s | %(message)s")
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-    return logger
 
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        "%(asctime)s | %(name)s | %(levelname)s | %(message)s",
+        datefmt="%H:%M:%S",
+    )
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.propagate = False
+    return logger
